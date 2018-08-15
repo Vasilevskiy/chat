4c7782af5df2e769f54bfe11cdfc5c81adce6420
Andrey Vasilevsky <andreyvasilevsky8@gmail.com>
2018-08-15 09:21:52 +0300

    #2 second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ChatWindowComponent} from './components/chat-window/chat-window.component';
+import {ChatInputComponent} from './components/chat-input/chat-input.component';
+import {UsersComponent} from './components/users/users.component';
+import {FormsModule} from '@angular/forms';
+import {ChatService} from './services/chat.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatWindowComponent,
+    ChatInputComponent,
+    UsersComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ChatService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
